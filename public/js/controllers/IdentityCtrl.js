a574ff51785390fdf889c739f9a43e82e2a784c1
Matt Vogel <matt@portlandwebdev.com>
2015-08-14 12:55:01 -0700

    updating classes for inputs, displaying data, making external calls

--- public/js/controllers/IdentityCtrl.js
+++ public/js/controllers/IdentityCtrl.js
@@ -1,5 +1,4 @@
 angular.module('IdentityCtrl', []).controller('IdentityController', ['$scope', '$http', 'usSpinnerService', 'Identity', function($scope, $http, usSpinnerService, Identity) {
-    
     $scope.startSpin = function(){
         usSpinnerService.spin('spinner-1');
     }
@@ -8,8 +7,9 @@ angular.module('IdentityCtrl', []).controller('IdentityController', ['$scope', '
     }
     $scope.go = function(item) {
       usSpinnerService.spin('spinner-1');
-	  $http.post('http://yadaproject.com/api/getCounts').then(function(resp) {
+      $http.get($scope.address).then(function(resp) {
         usSpinnerService.stop('spinner-1');
+        console.log(resp.data.data);
         $scope.identity = resp.data;
       }, function(err) {
         console.error('ERR', err);
